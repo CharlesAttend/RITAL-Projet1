f096a01776a86e1a23d52687152c963af820c30c
Charles <charles.vin@outlook.fr>
2023-02-21 18:48:07 +0100

    Feature: saving results working ✅

--- Review_classif/src/eval.py
+++ Review_classif/src/eval.py
@@ -4,21 +4,54 @@ from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report, f1_score, roc_auc_score
 from sklearn.feature_extraction.text import CountVectorizer
 from src.data import Custom_analyzer
+import pandas as pd
+import json, os
 
 def print_score(y_test, pred, name):
+    macro_f1 = f1_score(y_test, pred, average='macro')
+    micro_f1 = f1_score(y_test, pred, average='micro')
+    macro_auc = roc_auc_score(y_test, pred)
+    micro_auc = roc_auc_score(y_test, pred, average="weighted")
+    classif_report = classification_report(y_test, pred, output_dict=True)
+
     reports = f"""
-    Macro F1-score : {f1_score(y_test, pred, average='macro')}
-    Micro F1-score : {f1_score(y_test, pred, average='micro')}
-    Macro ROC-AUC: {roc_auc_score(y_test, pred)}
-    Weighted ROC-AUC: {roc_auc_score(y_test, pred, average="weighted")}
+    {name} :
+    =====
+    Macro F1-score : {macro_f1}
+    Micro F1-score : {micro_f1}
+    Macro ROC-AUC: {macro_auc}
+    Weighted ROC-AUC: {micro_auc}
     Classification report :
     {classification_report(y_test, pred)}
+    =====
     """
-    print(name, ':')
     print(reports)
-    return reports
+
+    classif_report['macro_auc'] = macro_auc
+    classif_report['micro_auc'] = micro_auc
+    return classif_report
 
 def fit_eval(X_train, y_train, X_test, y_test, balanced=None):
+    """
+    Fit et évalue les algo classique de classification sur les jeux de données envoyé en paramètre
+
+    Parameters
+    ----------
+    X_train: Sparce Matrix
+        Important : Une matrice BoW est attendu
+
+    X_test: Sparce Matrix
+        Important : Une matrice BoW est attendu
+
+    y_train: list
+        Label du train
+
+    y_test: list 
+        Label du test
+    
+    balanced: bool
+
+    """
     #Naïve Bayes
     if balanced==None:
         nb_clf = MultinomialNB()
@@ -40,12 +73,14 @@ def fit_eval(X_train, y_train, X_test, y_test, balanced=None):
     pred_svm = svm_clf.predict(X_test)
 
     # Ridge Classifier ?
+    results = (print_score(y_test, pred_nb, 'Naïve Bayes'),\
+        print_score(y_test, pred_lr, 'Logistic Regression'),\
+        print_score(y_test, pred_svm, 'SVM'))
+    algo_names = ['Naïve Bayes', 'Logistic Regression', 'SVM']
 
-    print_score(y_test, pred_nb, 'Naïve Bayes')
-    print_score(y_test, pred_lr, 'Logistic Regression')
-    print_score(y_test, pred_svm, 'SVM')
+    return results, algo_names
 
-def eval_from_config(X_train, X_test, y_train, y_test, config_name, vectorizer_class=CountVectorizer):
+def eval_from_config(X_train, X_test, y_train, y_test, config_name, vectorizer_class=CountVectorizer, saveName=None):
     """
     Evalue avec les prétraitement de la config sur les sets de train et test fournis en paramètre
 
@@ -68,8 +103,71 @@ def eval_from_config(X_train, X_test, y_train, y_test, config_name, vectorizer_c
     
     vectorizer_class: Submodule of sklearn.feature_extraction.text => {CountVectorizer, TfidfVectorizer, HashingVectorizer}
         This analyser will be plug into one of the sklearn.feature_extraction.text vectorizer with the param "analyzer = Mixed_anayzer"
+    
+    saveName: str
+        If not None, save result and config under this name
     """
-    vectorizer = vectorizer_class(analyzer = Custom_analyzer(config_name))
+    custom_ana = Custom_analyzer(config_name)
+    vectorizer = vectorizer_class(analyzer = custom_ana)
     X_train = vectorizer.fit_transform(X_train)
     X_test = vectorizer.transform(X_test)
-    fit_eval(X_train, y_train, X_test, y_test)
+    results, algo_names = fit_eval(X_train, y_train, X_test, y_test)
+    if saveName != None:
+        config = custom_ana.config
+        save_eval_and_config(config, results, algo_names, saveName)
+
+def save_eval_and_config(config, results, algo_names, saveName, path='stats/'):
+    """
+    Ajoute les résultat à la dataframe au format long situé dans path+"stats.csv"
+    Save la config sous le saveName fournis en param
+
+    Parameters
+    ----------
+    config: dict
+        La config du CustomAnalyzer 
+    
+    results: tuple of dict
+        Résultat renvoyé par fit_eval() avec toutes les metrics, il est possible d'en ajouter comme cela fut fait avec le AUC score
+    
+    algo_names: list of strings
+        Comme results est un tuple de résultat pour chaque algo, ce paramètre permet de nommer les algos dans l'ordre
+
+    saveName:
+        Le nom court pour la config associé, il doit être unique par rapport à un précédent, sous peine de remplacer la config de l'ancien
+    
+    path: Optional string
+        Optional path
+    """
+    # Sanity check
+    assert len(results) == len(algo_names)
+    assert os.path.isdir(path), "Wrong path to save configs"
+    
+    # Saving configs
+    with open(path+'config_name_map.json', 'r') as f:
+        # Reading
+        content = json.load(f)
+        assert isinstance(content, dict), "Problème avec le format du json, il faut un dictionnaire à la racine"
+    
+    # Checking if this saveName is not already used
+    if content.get(saveName, None) != None:
+        raise ValueError("Save Name already used")
+
+    # Rewrite
+    with open(path+'config_name_map.json', 'w') as f:
+        # Append
+        content[saveName] = config
+        # Rewrite
+        json.dump(content, f, indent=4)
+
+    # Saving Result
+    try:
+        df = pd.read_csv(path+"stats.csv")
+    except pd.errors.EmptyDataError:
+        df = pd.DataFrame()
+
+    for result, name in zip(results, algo_names):
+        tmp = pd.DataFrame(result).melt()
+        tmp['Algo'] = [name for _ in range(len(tmp))]
+        tmp['configName'] = saveName
+        df = pd.concat([df,tmp])
+    df.to_csv(path+"stats.csv")
