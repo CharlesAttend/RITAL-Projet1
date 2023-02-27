b9f86fccdc821a4358e5e4d51c89080aab87187c
dataymeric <13287345+dataymeric@users.noreply.github.com>
2023-02-27 19:15:45 +0100

    feat: ouaaais gridsearch + pipeline

---
+++ configs/test.py
@@ -0,0 +1,25 @@
+import string
+
+def lemmatizer():
+    pass
+
+def stemmer():
+    pass
+
+config = {
+    "base_vectorizer_param": {
+        "strip_accents": None,
+        "lowercase": True,
+        "stop_words": None,
+        "token_pattern": r"(?u)\b\w\w+\b",
+        "ngram_range": (1, 2),
+        "max_df": 1.0,
+        "min_df": 1,
+        "max_features": None, 
+        "binary": False,
+    },
+    "number": None, # Dictionnaire ou None
+    "ponctuation": None,  # !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~
+    "stemmer": None,
+    "lemmatizer": None,
+}
